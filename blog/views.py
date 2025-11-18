f814969e97828e0a69ca9a54dd57f531fb3e8c06
salah46 <garroudjimohamedse@gmail.com>
2025-11-18 17:37:03 +0100

    add post share functionality

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
-from django.core.paginator import Paginator,EmptyPage,InvalidPage,PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, InvalidPage, PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
+from .forms import EmailPostForm
+
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    queryset = Post.objects.filter(status=Post.Status.PUBLISHED)
+    context_object_name = 'posts'
+    paginate_by = 1
+    template_name = 'blog/post/list.html'
+
+
 def post_list(request):
     try:
         post_list = Post.objects.filter(status=Post.Status.PUBLISHED)
@@ -11,19 +24,19 @@ def post_list(request):
         # Pagination with 3 posts per page
         paginator = Paginator(post_list, 1)
         page_number = request.GET.get('page', 1)
-        
+
         posts = paginator.page(page_number)
     except EmptyPage:
      # If page_number is out of range get last page of results
         posts = paginator.page(paginator.num_pages)
     except PageNotAnInteger:
-        posts=paginator.page(1)
+        posts = paginator.page(1)
 
     return render(
-    request,
-    'blog/post/list.html',
-    {'posts': posts}
-)
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
 
 
 def post_detail(request, year, month, day, post):
@@ -35,3 +48,49 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day)
     return render(request, "blog/post/details.html", {"post": post})
+
+
+def post_share(request, post_id):
+    # retireive post by id
+
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+
+    )
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed vaidation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request,
+                  'blog/post/share.html',
+                  {
+                      'post': post,
+                      'form': form,
+                      'sent': sent})
--- blogsite/settings.py
+++ blogsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2qvaa1^a5p2mp_$2w3cpe6=bg7oc6r!88kjh(zr&j8iyg(1we#'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,3 +122,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Email server configuration 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
